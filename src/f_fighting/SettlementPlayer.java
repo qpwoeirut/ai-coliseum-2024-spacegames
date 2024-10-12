c736f8bcfbb1da2b6871f692428df6355f9d14c4
qpwoeirut <stanleyzhong8@gmail.com>
2024-10-11 20:18:40 -0700

    Create f_fighting player and delete old zip

---
+++ src/f_fighting/SettlementPlayer.java
@@ -0,0 +1,15 @@
+package f_fighting;
+
+import aic2024.user.UnitController;
+
+public class SettlementPlayer extends BasePlayer {
+    SettlementPlayer(UnitController uc) {
+        super(uc);
+    }
+
+    void run() {
+        while (true) {
+            uc.yield();
+        }
+    }
+}
